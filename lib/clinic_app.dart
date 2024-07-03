eb5b649e7b7cb7780d7c99542d4b11380c762651
DESKTOP-VCIBF5D\DELL <abdelrahmantemsah30@gmail.com>
2024-07-03 15:41:42 +0300

    change in postion folders

---
+++ lib/clinic_app.dart
@@ -0,0 +1,27 @@
+import 'package:clinic_system/core/constant/routes.dart';
+import 'package:clinic_system/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ClinicApp extends StatelessWidget {
+  const ClinicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(375, 821),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        // Use builder only if you need to use library outside ScreenUtilInit context
+        builder: (_, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primaryColor: ColorsApp.primary,
+              scaffoldBackgroundColor: ColorsApp.white,
+            ),
+            onGenerateRoute: Routes.generateRoute,
+          );
+        });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,8 @@
-import 'package:clinic_system/core/constant/routes.dart';
-import 'package:clinic_system/core/theme/colors.dart';
+import 'package:clinic_system/clinic_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const ClinicApp());
 }
 
-class ClinicApp extends StatelessWidget {
-  const ClinicApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(375, 821),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        // Use builder only if you need to use library outside ScreenUtilInit context
-        builder: (_, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primaryColor: ColorsApp.primary,
-              scaffoldBackgroundColor: ColorsApp.white,
-            ),
-            onGenerateRoute: Routes.generateRoute,
-          );
-        });
-  }
-}
