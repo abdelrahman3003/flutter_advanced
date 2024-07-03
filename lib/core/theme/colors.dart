1450157b5883056ab8b5fb046bf8c587477359f5
DESKTOP-VCIBF5D\DELL <abdelrahmantemsah30@gmail.com>
2024-07-03 19:26:14 +0300

    preparing to project

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ColorsApp {
+class AppColors {
   static const Color primary = Color(0xff247CFF);
   static const Color white = Colors.white;
+  static const Color black = Colors.black;
 }
---
+++ lib/core/theme/styles.dart
@@ -0,0 +1,16 @@
+import 'package:clinic_system/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font24black700w = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+    static TextStyle font32blueBold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+}
