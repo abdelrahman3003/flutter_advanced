0912819059b4721cc3aedb34b96d3e99a3609174
DESKTOP-VCIBF5D\DELL <abdelrahmantemsah30@gmail.com>
2024-08-09 15:45:05 +0300

    create login screen

--- lib/core/constant/routes.dart
+++ lib/core/constant/routes.dart
@@ -1,13 +1,16 @@
+import 'package:clinic_system/features/auth/signIn/view/signIn_view.dart';
 import 'package:clinic_system/features/onboarding/view/onboarding_view.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   static const String konboardingView = '/onboardingView';
-  static const String klogin = '/login';
- static Route generateRoute(RouteSettings setting) {
+  static const String ksingin = '/singin';
+  static Route generateRoute(RouteSettings setting) {
     switch (setting.name) {
       case "/":
         return MaterialPageRoute(builder: (context) => const OnboardingView());
+      case ksingin:
+        return MaterialPageRoute(builder: (context) => const SigninView());
       default:
         return MaterialPageRoute(
           builder: (context) => Scaffold(
